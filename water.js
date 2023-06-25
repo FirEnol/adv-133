8093eee4bf018a9fd76ada9bf804c4228c3d4517
FirEnol <84876373+FirEnol@users.noreply.github.com>
2023-06-25 10:47:04 -0700

    Add files via upload

---
+++ water.js
@@ -0,0 +1,41 @@
+Status = "";
+water_image = "";
+objects = [];
+
+function preload(){
+    water_image = loadImage("water.jpeg");
+}
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded! ");
+    Status = true;
+    object_Detector.detect(water_image,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw(){
+    image(water_image,0,0,600,400);
+    if(Status != ""){
+        for(i=0; i < objects.length;i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%",objects[i].x, objects[i].y);
+       noFill();
+       stroke("#fc0303");
+       rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       
+        }
+    }
+}
